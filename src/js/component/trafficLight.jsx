bbea0ae7b8cd4da9bf7aff3e21247478307272d2
labs404 <136141267+labs404@users.noreply.github.com>
2023-07-29 00:56:29 +0000

    criteria met for base project submission

---
+++ src/js/component/trafficLight.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const TrafficLight = () => {
+    const [currentLight, setCurrentLight] = useState("");
+
+    return (
+        <>
+            <div className="traffic-light-stem"></div>
+            <div className="light-container">
+                <div className={currentLight == "red" ? "red-light selected-light" : "red-light"} onClick={currentLight == "red" ? () => setCurrentLight("") : () => setCurrentLight("red")}>red</div>
+                <div className={currentLight == "yellow" ? "yellow-light selected-light" : "yellow-light"} onClick={currentLight == "yellow" ? () => setCurrentLight("") : () => setCurrentLight("yellow")}>yellow</div> 
+                <div className={currentLight == "green" ? "green-light selected-light" : "green-light"} onClick={currentLight == "green" ? () => setCurrentLight("") : () => setCurrentLight("green")}>green</div>
+            </div>
+        </>
+    );
+};
+
+export default TrafficLight;
